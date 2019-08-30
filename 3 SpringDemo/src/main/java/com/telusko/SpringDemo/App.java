969045860daf5b1216875fdcf0992ffb4688a5d2
Telusko <Telusko@SuperComputer>
2019-08-30 15:20:24 +0530

    BeanFactory

---
+++ 3 SpringDemo/src/main/java/com/telusko/SpringDemo/App.java
@@ -0,0 +1,15 @@
+package com.telusko.SpringDemo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	Alien obj = (Alien)factory.getBean("alien");
+    	obj.code();
+    }
+}
