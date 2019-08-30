e0215eaafb1a4453fc314c75d91d3ee61dd042b6
Telusko <Telusko@SuperComputer>
2019-08-30 15:21:29 +0530

    ApplicationContext

---
+++ 4 SpringDemo/src/main/java/com/telusko/SpringDemo/Alien.java
@@ -0,0 +1,8 @@
+package com.telusko.SpringDemo;
+
+public class Alien {
+public void code() 
+{
+	System.out.println("Im Coding..");
+}
+}
---
+++ 4 SpringDemo/src/main/java/com/telusko/SpringDemo/App.java
@@ -0,0 +1,18 @@
+package com.telusko.SpringDemo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+    	Alien obj = (Alien)factory.getBean("alien");
+    	obj.code();
+    }
+}
