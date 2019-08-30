3533b3b27bcd325f33262547872547285786d29e
Telusko <Telusko@SuperComputer>
2019-08-30 15:16:21 +0530

    Dependency Injection in Spring

---
+++ 1 firstproj/src/main/java/com/telusko/first/Alien.java
@@ -0,0 +1,13 @@
+package com.telusko.first;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien 
+{
+public void code()
+{
+	System.out.println("Im Coding");
+}
+}
+
