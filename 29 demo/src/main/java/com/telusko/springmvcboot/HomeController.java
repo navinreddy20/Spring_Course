a2be0dddaf13060a06097abb03832ef8e397077d
Telusko <Telusko@SuperComputer>
2019-08-30 16:40:22 +0530

    Spring Data JPA Configuration

---
+++ 29 demo/src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -0,0 +1,66 @@
+package com.telusko.springmvcboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.telusko.springmvcboot.model.Alien;
+
+@Controller
+public class HomeController
+{
+	@ModelAttribute
+	public void modelData(Model m) 
+	{
+	m.addAttribute("name","Aliens");
+	}
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "index";
+	}
+	
+	@GetMapping("getAliens")
+	public String getAliens(Model m)
+	{
+		List<Alien> aliens= Arrays.asList(new Alien(101,"Navin"), new Alien(102,"Rose"));
+		m.addAttribute("result",aliens);
+		return "showAliens";
+		
+	}
+	
+	@GetMapping("getAlien")
+	public String getAlien(@RequestParam int aid, Model m)
+	{
+		List<Alien> aliens= Arrays.asList(new Alien(101,"Navin"), new Alien(102,"Rose"));
+		m.addAttribute("result",new Alien(aid,"Navin"));
+		
+		return "showAliens";
+		
+	}
+	
+	@PostMapping(value="addAlien")
+	public String addAlien(@ModelAttribute Alien a)
+	{
+		
+	
+		return "result";
+		
+	}
+}
+
+
---
+++ 29 demo/src/main/webapp/views/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+ 	Welcome to Telusko
+ 	
+ 	<form action="addAlien" method="post">
+ 		Enter your id : <input type="text" name="aid"><br>
+ 		Enter your name : <input type="text" name="aname"><br>
+ 		<input type="submit">
+ 	</form>
+ 	
+ 	
+</body>
+</html>
