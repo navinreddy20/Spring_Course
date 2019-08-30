0dc339b4230f8d01b21ee967958f40b2e6d5db1e
Telusko <Telusko@SuperComputer>
2019-08-30 15:36:28 +0530

    Creating Controller

---
+++ 13 demo/src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -0,0 +1,14 @@
+package com.telusko.springmvcboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+	@RequestMapping("/")
+	public String home()
+	{
+		return "index.jsp";
+	}
+}
