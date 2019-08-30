f5a07398e5f34d034e9c31071e1cacc59bfa133f
Telusko <Telusko@SuperComputer>
2019-08-30 16:35:12 +0530

    Spring MVC Part 2

---
+++ 25 springmvc/src/main/java/com/telusko/springmvc/model/Alien.java
@@ -0,0 +1,26 @@
+package com.telusko.springmvc.model;
+
+public class Alien 
+{
+	private int aid;
+	private String aname;
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aname=" + aname + "]";
+	}
+	
+	
+}
