05cceac3dc1f38375c2551602dbddfc6608d2159
Telusko <Telusko@SuperComputer>
2019-08-30 15:23:38 +0530

    Spring Container

---
+++ 5 SpringDemo/src/main/java/com/telusko/SpringDemo/Alien.java
@@ -0,0 +1,14 @@
+package com.telusko.SpringDemo;
+
+public class Alien
+{ 
+	int age;
+	
+	public Alien() {
+		System.out.println("Alien Object Created..");	
+	}
+public void code() 
+{
+	System.out.println("Im Coding..");
+}
+}
