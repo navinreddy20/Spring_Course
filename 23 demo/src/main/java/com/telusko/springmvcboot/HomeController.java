7148e7d77e0e74d5282c22a12a9ed07bd93a4451
Telusko <Telusko@SuperComputer>
2019-08-30 16:31:36 +0530

    Post Mapping

---
+++ 23 demo/src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -0,0 +1,56 @@
+package com.telusko.springmvcboot;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.telusko.springmvcboot.model.Alien;
+
+@Controller
+public class HomeController
+{
+	@ModelAttribute
+	public void modelData(Model m) 
+	{
+	m.addAttribute("name","Aliens");
+	}
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "index";
+	}
+	
+	
+	@RequestMapping("add")
+	public String add(@RequestParam("num1")int i ,@RequestParam("num2") int j, Model m)
+	{
+		
+	
+		int num3 = i + j;
+		
+		m.addAttribute("num3",num3);
+		
+		return  "result";
+		
+	}
+	
+	@PostMapping(value="addAlien")
+	public String addAlien(@ModelAttribute Alien a)
+	{
+		
+	
+		return "result";
+		
+	}
+}
